453f5292cff67487a68c4aa9859afe4bdd993e67
Virajisesiri <viraji.sesiri@gmail.com>
2023-12-18 20:22:49 +0530

    complete isDataValid()

--- src/main/java/lk/ijse/dep11/pos/controller/ManageCustomerFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/ManageCustomerFormController.java
@@ -101,6 +101,22 @@ public class ManageCustomerFormController {
         if(!isDataValid()) return;
     }
 
+    private boolean isDataValid() {
+        String name = txtCustomerName.getText().strip();
+        String address = txtCustomerAddress.getText().strip();
+
+        if(!name.matches("[A-Za-z ]{2,}")){
+            txtCustomerName.requestFocus();
+            txtCustomerName.selectAll();
+            return false;
+        }else if(address.length() < 3){
+            txtCustomerAddress.requestFocus();
+            txtCustomerAddress.selectAll();
+            return false;
+        }
+        return true;
+    }
+
     public void btnDelete_OnAction(ActionEvent actionEvent) {
     }
 }
